2cce367d6c218563f3d9c58fa8cf8d001a1c66d0
tho888 <105895875+tho888@users.noreply.github.com>
2024-01-13 14:26:03 +1300

    pScope init commit

--- my_phenakistoscope.js
+++ my_phenakistoscope.js
@@ -1,49 +1,99 @@
-const SLICE_COUNT = 10;
+const SLICE_COUNT = 11;
 
-function setup_pScope(pScope){
+function setup_pScope(pScope) {
   pScope.output_mode(ANIMATED_DISK);
   pScope.scale_for_screen(true);
   pScope.draw_layer_boundaries(false);
   pScope.set_direction(CCW);
   pScope.set_slice_count(SLICE_COUNT);
+  pScope.load_image_sequence("kiwi", "png", 11);
+  pScope.load_image_sequence("stars", "png", 11);
 }
 
-function setup_layers(pScope){
+function setup_layers(pScope) {
 
-  new PLayer(null, 220);  //lets us draw the whole circle background, ignoring the boundaries
+  new PLayer(null, 5, 32, 51);
 
-  var layer1 = new PLayer(faces);
-  layer1.mode( SWIRL(5) );
-  layer1.set_boundary( 200, 1000 );
+  var layer1 = new PLayer(earth);
+  layer1.mode(RING);
+  layer1.set_boundary(0, 1000);
 
-  var layer2 = new PLayer(squares);
-  layer2.mode( RING );
-  layer2.set_boundary( 0, 400 );
+  var layer2 = new PLayer(grass);
+  layer2.mode(RING);
+  layer2.set_boundary(475, 500);
+
+  var layer3 = new PLayer(bug);
+  layer3.mode(SWIRL(2));
+  layer3.set_boundary(100, 425);
+
+  var layer4 = new PLayer(kiwi);
+  layer4.mode(RING);
+  layer4.set_boundary(0, 1000);
+
+  var layer5 = new PLayer(stars);
+  layer5.mode(RING);
+  layer5.set_boundary(0, 1000);
+
+  var layer6 = new PLayer(outerRing);
+  layer6.mode(RING);
+  layer6.set_boundary(965, 1000);
 }
 
-function faces(x, y, animation, pScope){
-  
-  scale(animation.frame*2);
-
-  ellipse(0,0,50,50); // draw head
-  fill(30);
-  ellipse(-10,-10,10,10); //draw eye
-  ellipse(10,-10,10,10); // draw eye
-  arc(0,10,20,10,0,180); // draw mouth
-
-}
-
-function squares(x, y, animation, pScope){
-
-  // this is how you set up a background for a specific layer
+function earth(x, y, animation, pScope) {
   let angleOffset = (360 / SLICE_COUNT) / 2
   let backgroundArcStart = 270 - angleOffset;
   let backgroundArcEnd = 270 + angleOffset;
 
-  fill(66, 135, 245)
-  arc(x,y,800,800,backgroundArcStart,backgroundArcEnd); // draws "pizza slice" in the background
+  fill(35, 15, 0)
+  arc(x, y, 1000, 1000, backgroundArcStart, backgroundArcEnd);
+}
 
-  fill(255)
-  rect(-10,-300-animation.wave()*50,20,20) // .wave is a cosine wave btw
+function grass(x, y, animation, pScope) {
+  pScope.fill_background(45, 180, 0, 100);
+}
 
+function bug(x, y, animation, pScope) {
+  scale(animation.frame);
+
+  var bugX = animation.wave(2) * -100;
+  var bugR = animation.wave(2) * 35;
+
+  translate(bugX + 110, 0);
+  rotate(bugR + 180);
+  stroke(230, 89, 98);
+  strokeWeight(5)
+  fill(255, 132, 137);
+
+  beginShape();
+    curveVertex(10, 0);
+    curveVertex(20, 10);
+    curveVertex(20, 30);
+    curveVertex(40, 50);
+    curveVertex(40, 60);
+    curveVertex(20, 80);
+    curveVertex(20, 90);
+    curveVertex(40, 110);
+    curveVertex(30, 120);
+    curveVertex(20, 110);
+    curveVertex(0, 90);
+    curveVertex(0, 80);
+    curveVertex(20, 60);
+    curveVertex(20, 50);
+    curveVertex(0, 30);
+    curveVertex(0, 10);
+  endShape(CLOSE);
+}
+
+function kiwi(x, y, animation, pScope) {
+  scale(1.2);
+  pScope.draw_image_from_sequence("kiwi", 0, -523, animation.frame);
+}
+
+function stars(x, y, animation, pScope) {
+  scale(1);
+  pScope.draw_image_from_sequence("stars", 0, -820, animation.frame);
+}
+
+function outerRing(x, y, animation, pScope) {
+  pScope.fill_background(0, 75, 130);
 }
